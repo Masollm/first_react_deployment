ae7b0e72b2550eb4d8db64085d3c75ac3fb067d3
Martin <martinsollmann@gmail.com>
2021-04-19 16:38:37 +0200

    'changes'

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           
-        Hello there!
+        Hello there, breakin changes!
         </p>
       </header>
     </div>
